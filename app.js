29e5ec699dc9e730a6301038d82ed69c8876905c
COR Students <corstudents@Matts-MBP-97.localdomain>
2018-12-13 08:30:08 -0500

    enter

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const bodyparser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+let db
+
+
+
+app.use('view engine','pug')
+app.get()
+app.post()
+
+
+
+app.listen(3000,function () {
+    
+})
